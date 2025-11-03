f91eaa98fe8d289e07c87bcf7945355ef3d99920
niall.garden <cellarinfo@gmail.com>
2025-11-03 16:33:23 +0000

    Quartz sync: Nov 3, 2025, 4:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "niall.garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
