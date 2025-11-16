1d1f9690a4c55f1df209f915179c7bb73c2eb6b9
niall.garden <cellarinfo@gmail.com>
2025-11-16 09:59:41 +0000

    back to old sidebar list for now

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,11 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.RecentNotes({
-  title: "Recent posts",
-  limit: 100,
-  showTags: false,
-})
+    Component.Explorer()
 ,
   ],
   right: [
